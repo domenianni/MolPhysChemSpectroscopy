120b54ff3da0712c2be7d1488320906d3ee26ac6
Markus Bauer <markusbauer97web.de>
2024-01-18 00:48:18 +0100

    Added "coreAbstractAxis.py" to serve as the base class for axis objects holding energy/time of a measurement.

---
+++ pySpec/SpecCore/SpecCoreAxis/coreAbstractAxis.py
@@ -0,0 +1,133 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class AbstractAxis(ABC):
+    """
+    :param array: The Array to be wrapped within the class. Has to be passed to the class by the constructor of the
+        subclass.
+
+    Abstract Base Class for creating axes, utilised in the :class:`Spectrum`, :class:`Transient` and
+    :class:`TransientSpectrum` classes. Implements most of the arithmetic interface and indexing, to be used like numpy
+    arrays, though the arithmetic and indexing methods returns the array directly and the array itself can also be
+    accessed directly by the `.array` property.
+    Supports addition, subtraction, multiplication and division.
+    """
+
+    __slots__ = ("_array", "_unit")
+
+    def __init__(self, array: np.ndarray, unit: str):
+
+        if len(array.shape) != 1:
+            raise ValueError("Axis Object only accepts one-dimensional arrays.")
+
+        self._array = array.copy()
+        self._unit = unit
+
+    def __getitem__(self, idx):
+        return self._array[idx]
+
+    def __iter__(self):
+        return iter(self._array)
+
+    def __len__(self):
+        return np.size(self._array)
+
+    def __neg__(self):
+        return self.__class__(- self._array, self._unit)
+
+    def __add__(self, other):
+        return self.__class__(self._array + other, self._unit)
+
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        return self.__class__(self._array - other, self._unit)
+
+    def __rsub__(self, other):
+        return self.__class__(other - self._array, self._unit)
+
+    def __mul__(self, other):
+        return self.__class__(self._array * other, self._unit)
+
+    __rmul__ = __mul__
+
+    def __truediv__(self, other):
+        return self.__class__(self._array / other, self._unit)
+
+    def __rtruediv__(self, other):
+        return self.__class__(other / self._array, self._unit)
+
+    @property
+    def size(self):
+        """
+        The length of the axis.
+        """
+        return np.size(self._array)
+
+    @property
+    def array(self):
+        """
+        The underlying np.array. Can be overwritten.
+        """
+        return self._array
+
+    @array.setter
+    def array(self, array):
+        """
+        Overwrites the underlying np.array.
+        """
+        self._array = array.copy()
+
+    @property
+    def unit(self):
+        """
+        The unit specified at the point of axis creation.
+        """
+        return self._unit
+
+    def closest_to(self, value):
+        """
+        Returns the closest match of the axis' array with `value` as a tuple of the index and the matching value.
+
+        :param value: The value to search for.
+        :type value: float
+        :return: tuple( index, value )
+        """
+        idx = int((np.abs(self._array - value)).argmin())
+        return idx, self._array[idx]
+
+    def sort_array(self) -> np.ndarray:
+        """
+        Sorts the array in increasing order and return the sorting mask.
+
+        :return: Sorting mask.
+        """
+        sorting_mask = np.argsort(self._array)
+
+        self._array = self._array[sorting_mask]
+        return sorting_mask
+
+    @abstractmethod
+    def shift_by(self, amount, anchor):
+        """
+        Abstract method to shift an axis by a certain amount. Necessary for EnergyAxis, since shifting the axis measured
+        with gratings is only linear in the Wavelength domain. Shifts inplace!
+
+        :param amount: Amount to shift by.
+        :type amount: float
+        :param anchor: Position to shift around.
+        :type anchor: float
+        """
+        pass
+
+    @abstractmethod
+    def convert_to(self, axis_type):
+        """
+        Abstract conversion function to convert axis into another axis type.
+
+        :param axis_type: Axis type to convert to.
+        :type axis_type: str
+        :return: New Axis Object.
+        """
+        pass
