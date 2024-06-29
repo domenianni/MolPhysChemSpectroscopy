b353646d1b1fc05af78d4b482ab06a2fa82cd39e
Markus Bauer <98280589+pentaphonic@users.noreply.github.com>
2024-06-29 13:46:26 +0200

    Implement updated Stitch Correction

--- pySpec/pyStitchCorr/stitchCorr.pyi
+++ pySpec/pyStitchCorr/stitchCorr.pyi
@@ -1,6 +1,13 @@
 import numpy as np
 
-def stitchCorr(x: np.ndarray, t: np.ndarray, y: np.ndarray, block_count: int, copy: bool) -> np.ndarray: ...
+def stitchCorr(x: np.ndarray,
+               t: np.ndarray,
+               y: np.ndarray,
+               block_count: int,
+               reference: int,
+               sortedInput: bool,
+               isAsymmetric: bool,
+               linear: bool) -> np.ndarray: ...
 # :param x, t, y: numpy-arrays from python call. No changes necessary.
 #
 # :param blockCount: Amount of stitching blocks utilised.
@@ -10,15 +17,15 @@ def stitchCorr(x: np.ndarray, t: np.ndarray, y: np.ndarray, block_count: int, co
 # :returns: numpy array of calculated offsets. y-values are changed also automatically, if copy=false.
 #
 
-def stitchCorrFunc(offset: np.ndarray, x: np.ndarray, y: np.ndarray) -> float: ...
-# Pure target function to be called from python frontend and minimized within this context. Only one timestep at
-# a time is processed.
-#
-# :param offset: Numpy array of offset values, which are added on top of DOD values.
-#
-# :param x, y: Numpy array of the respective wavelength and DOD values.
-#
-# :returns: Mean total variance (one double value).
-#
-# See documentation inside the class for more details on the evaluated function.
-#
+# def stitchCorrFunc(offset: np.ndarray, x: np.ndarray, y: np.ndarray) -> float: ...
+# # Pure target function to be called from python frontend and minimized within this context. Only one timestep at
+# # a time is processed.
+# #
+# # :param offset: Numpy array of offset values, which are added on top of DOD values.
+# #
+# # :param x, y: Numpy array of the respective wavelength and DOD values.
+# #
+# # :returns: Mean total variance (one double value).
+# #
+# # See documentation inside the class for more details on the evaluated function.
+# #
