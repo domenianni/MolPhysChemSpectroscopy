29571757a8ca27de87ab834577dfe54c2fc855f2
Markus Bauer <98280589+pentaphonic@users.noreply.github.com>
2024-06-29 13:48:09 +0200

    Add new Integral Class

--- pySpec/SpecAnalyze/__init__.py
+++ pySpec/SpecAnalyze/__init__.py
@@ -1,4 +1,5 @@
 from .analyzeIntegral import IntegralAnalysis
+from .analyzeIntegral_new import Integral
 from .analyzeProductSpectrum import ProductSpectrum
 from .analyzeStatistics import StatisticalAnalysisMeasurements
 from .NumericalFit import *
