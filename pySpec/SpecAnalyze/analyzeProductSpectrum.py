62e2dde4f28c138a6f16938f650a2a930a004f23
Markus Bauer <98280589+pentaphonic@users.noreply.github.com>
2024-07-08 14:30:27 +0200

    Inherits now from TransientSpectrum
    - .product property removed
    - amplitude now reflected in .static_data

--- pySpec/SpecAnalyze/analyzeProductSpectrum.py
+++ pySpec/SpecAnalyze/analyzeProductSpectrum.py
@@ -18,7 +18,7 @@ This file is part of pySpec
 
 from ..SpecCore.SpecCoreSpectrum.coreTransientSpectrum import TransientSpectrum
 from ..SpecCore.SpecCoreSpectrum.coreSpectrum import Spectrum
-from ..SpecCore.coreLineShapes import  gaussian
+from ..SpecCore.coreLineShapes import gaussian
 
 from copy import deepcopy
 import numpy as np
@@ -26,7 +26,7 @@ from scipy.integrate import trapezoid
 from scipy.signal import convolve
 
 
-class ProductSpectrum:
+class ProductSpectrum(TransientSpectrum):
 
     @property
     def amplitude(self):
@@ -34,6 +34,9 @@ class ProductSpectrum:
 
     @amplitude.setter
     def amplitude(self, value):
+        self._static_data.y *= value / self._amplitude
+        self.y = self._calculate_product()
+
         self._amplitude = value
 
     @property
@@ -42,6 +45,9 @@ class ProductSpectrum:
 
     @offset.setter
     def offset(self, value):
+        self._static_data.y += value - self._offset
+        self.y = self._calculate_product()
+
         self._offset = value
 
     @property
@@ -49,13 +55,13 @@ class ProductSpectrum:
         return self._fwhm
 
     @fwhm.setter
-    def fwhm(self, value):
+    def fwhm(self, value: float):
         if value < 0:
             raise ValueError("FWHM can only be positive!")
 
         self._fwhm = value
 
-        self._static_data.y = self._backup['static_data'].y.array
+        self._static_data.y = self._amplitude * self._backup['static_data'].y.array
 
         if value > 0:
             self._static_data.y = self.convolve_gaussian(self._static_data.x.array,
@@ -72,6 +78,7 @@ class ProductSpectrum:
 
         self._transient_data = deepcopy(self._backup['transient_data'])
         self._transient_data.x.shift_by(self._shift)
+        self.y = self._calculate_product()
 
     @property
     def transient_data(self):
@@ -81,22 +88,22 @@ class ProductSpectrum:
     def static_data(self):
         return self._static_data
 
-    @property
-    def product(self):
-        add_data = deepcopy(self._static_data)
-        add_data.interpolate_to(self._transient_data.x)
-        add_data.y = self._amplitude * add_data.y + self._offset
-
-        return TransientSpectrum(self._transient_data.x,
-                                 self._transient_data.t,
-                                 self._transient_data.y + add_data.y,
-                                 self._transient_data.x.unit,
-                                 self._transient_data.t.unit,
-                                 'ddod')
+    # @property
+    # def product(self):
+    #     add_data = deepcopy(self._static_data)
+    #     add_data.interpolate_to(self._transient_data.x)
+    #
+    #     return TransientSpectrum(self._transient_data.x,
+    #                              self._transient_data.t,
+    #                              self._transient_data.y + add_data.y,
+    #                              self._transient_data.x.unit,
+    #                              self._transient_data.t.unit,
+    #                              'ddod')
 
     def __init__(self,
                  transient_data: TransientSpectrum,
                  static_data: Spectrum):
+
         transient_data.orient_data('x')
 
         self._offset = 0
@@ -109,12 +116,24 @@ class ProductSpectrum:
 
         self._backup = {'transient_data': deepcopy(transient_data), 'static_data': deepcopy(static_data)}
 
-    @staticmethod
-    def convolve_gaussian(x: np.ndarray, y: np.ndarray, fwhm: float) -> np.ndarray:
-        x_gauss = x - np.mean(x)
-        y_gauss = gaussian(x_gauss, 1, 0, fwhm)
-        norm = trapezoid(np.nan_to_num(y), x)
-        y = convolve(np.nan_to_num(y), y_gauss, mode='same')
-        y *= norm / trapezoid(np.nan_to_num(y), x)
+        super().__init__(
+            self._transient_data.x,
+            self._transient_data.t,
+            self._calculate_product(),
+            self._transient_data.x.unit,
+            self._transient_data.t.unit,
+            'ddod'
+        )
 
-        return y
+    #@staticmethod
+    # def convolve_gaussian(x: np.ndarray, y: np.ndarray, fwhm: float) -> np.ndarray:
+    #     x_gauss = x - np.mean(x)
+    #     y_gauss = gaussian(x_gauss, 1, 0, fwhm)
+    #     norm = trapezoid(np.nan_to_num(y), x)
+    #     y = convolve(np.nan_to_num(y), y_gauss, mode='same')
+    #     y *= norm / trapezoid(np.nan_to_num(y), x)
+    #
+    #     return y
+
+    def _calculate_product(self):
+        return self._transient_data.y + self._static_data.interpolate_to(self._transient_data.x, inplace=False).y
