8be4a14632109222759020371470b3eb0d887147
Markus Bauer <98280589+pentaphonic@users.noreply.github.com>
2024-08-19 11:42:10 +0200

    Add MPCFigure

---
+++ pySpec/SpecPlot/__init__.py
@@ -0,0 +1,5 @@
+# from .plotStaticMethods import create_colormap, lighten_color, exponential_string, factor_string
+# from .plotMinorSymLogLocator import MinorSymLogLocator
+# from .plotFluorescence import plotFluorescence
+# from .plotTransSpec import plotTransSpec, plotProdSpec
+from .plotMPCFigure import MPCFigure
