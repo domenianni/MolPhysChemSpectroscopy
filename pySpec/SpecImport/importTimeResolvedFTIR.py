9a9ea0b4a33523b6b6032e1a3617f3c473cd578f
Markus Bauer <98280589+pentaphonic@users.noreply.github.com>
2025-02-07 16:24:18 +0100

    Add function descriptions

--- pySpec/SpecImport/importTimeResolvedFTIR.py
+++ pySpec/SpecImport/importTimeResolvedFTIR.py
@@ -20,8 +20,6 @@ import numpy as np
 import concurrent.futures as ftr
 from difflib import SequenceMatcher
 from copy import deepcopy
-
-from scipy.optimize import minimize
 from scipy.stats import linregress
 
 from ..SpecCore.coreParser import Parser
@@ -42,6 +40,14 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
     """
 
     def __init__(self, data_list, pre_scans_amount: int = 1, pre_scans_offset: int = 0, file_list: str = None):
+        """
+        Initializes the ImportTimeResolvedFTIR object with provided data and parameters.
+
+        :param data_list: List of data sets (TransientSpectrum objects) to be processed.
+        :param pre_scans_amount: Number of pre-scans to consider for delta-OD calculation.
+        :param pre_scans_offset: Time offset for the pre-scans.
+        :param file_list: List of file names associated with the data sets.
+        """
         super().__init__(data_list, file_list)
 
         self._pre_scans_amount = pre_scans_amount
@@ -53,18 +59,38 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
         self._calculate_dod()
 
     def __len__(self):
+        """
+        Returns the number of intensity spectra available.
+
+        :returns: Length of the intensities list.
+        """
         return len(self._intensities_list)
 
     @property
     def intensities(self):
+        """
+        Returns the list of intensity spectra.
+
+        :returns: List of intensity data (TransientSpectrum objects).
+        """
         return self._intensities_list
 
     @property
     def intensities_average(self):
+        """
+        Returns the average of the intensities data.
+
+        :returns: Averaged TransientSpectrum object.
+        """
         return TransientSpectrum.average_from(self._intensities_list)
 
     @property
     def spectra(self):
+        """
+        Returns the calculated spectra (either intensity spectra or solvent-corrected spectra).
+
+        :returns: List of Spectra objects (solvent-corrected if applicable).
+        """
         if self._solvent is None:
             return self._intensities_list
         else:
@@ -72,34 +98,72 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
 
     @property
     def spectra_average(self):
+        """
+        Returns the averaged spectra.
+
+        :returns: Averaged TransientSpectrum object of the spectra.
+        """
         return TransientSpectrum.average_from(self.spectra)
 
     def set_solvent_reference(self, path, **kwargs):
+        """
+        Sets the solvent reference spectrum by reading and interpolating it.
+
+        :param path: Path to the solvent reference file.
+        :param kwargs: Additional arguments passed to Spectrum.average_from_files.
+        :returns: The current ImportTimeResolvedFTIR object with solvent reference set.
+        """
         self._solvent = Spectrum.average_from_files(path, **kwargs)
-        self._solvent.interpolate_to(self._intensities_list[0].x)
+        self._solvent.interpolate_to(self._intensities_list[0].x, inplace=True)
 
         return self
 
     def __singular_timeaxis(self, time_axis: TimeAxis):
+        """
+        Sets the same time axis for all data sets.
+
+        :param time_axis: The TimeAxis object to assign to all data sets.
+        """
         for data in self._data_list:
             data.t = time_axis
 
     def __averaged_timeaxis(self, time_axes):
+        """
+        Averages multiple time axes and assigns the result to all data sets.
+
+        :param time_axes: List of TimeAxis objects to average.
+        """
         axis = TimeAxis.from_average(time_axes)
 
         for data in self._data_list:
             data.t = axis
 
     def __individual_timeaxis(self, time_axes, paths, match):
+        """
+        Assigns time axes individually to each data set based on file names and matching criteria.
+
+        :param time_axes: List of TimeAxis objects.
+        :param paths: List of file paths.
+        :param match: Whether to match file names with time axes.
+        """
         if match:
             for t, name in zip(time_axes, paths):
-                idx = np.argmax([SequenceMatcher(None, name.name, data_name.name).ratio() for data_name in self._file_list])
+                idx = np.argmax(
+                    [SequenceMatcher(None, name.name, data_name.name).ratio() for data_name in self._file_list]
+                )
                 self._data_list[idx].t = t
         else:
             for data, t in zip(self._data_list, time_axes):
                 data.t = t
 
     def timeaxis_from(self, path, match=True):
+        """
+        Assigns the time axis to the data sets based on files provided in the specified path.
+
+        :param path: Path(s) to files containing time axis information.
+        :param match: Whether to match file names with data sets.
+        :returns: The current ImportTimeResolvedFTIR object with assigned time axis.
+        """
         # Assign the time axis read in from (a) supplied path(s).
         # First parse the supplied path
         paths = Parser.parse_path(path)
@@ -126,6 +190,12 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
         return self
 
     def define_timeaxis(self, step_size):
+        """
+       Defines a custom time axis for the data sets based on the provided step size.
+
+       :param step_size: The time step size for the time axis.
+       :returns: The current ImportTimeResolvedFTIR object with the defined time axis.
+       """
         t = TimeAxis.from_parameters(steps          = len(self._data_list[0].t),
                                      step_size      = step_size,
                                      time_zero_step = self._pre_scans_amount + self._pre_scans_offset,
@@ -137,6 +207,11 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
         return self
 
     def _calculate_dod(self):
+        """
+        Calculates the delta-OD (optical density change) for the intensity spectra.
+
+        :raises: Prints message if delta-OD is already computed.
+        """
         if not self.__dod:
             for data in self._data_list:
                 data.orient_data('t')
@@ -149,13 +224,12 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
 
     def correct_drift(self, t_region: float, position: int or str or slice, drift_type: str = 'individual'):
         """
-        Uses the defined `position` to correct intensity loss due to diffusion- or pump-effects. starting at
-        `start_time`. The position uses the same syntax as the `transient` and `spectrum` properties of
-        :class:`TransientSpectrum`, so `int` for an index, `slice` for a region, which will be averaged, and `string`
-        for an x-position.
+        Corrects drift in intensity data by normalizing the spectra over a defined time region.
 
-        :param t_region: The time-point used for the drift-correction and also the starting time for the correction.
-        :param position: The position which can be used to correct for an occurring drift.
+        :param t_region: Time region to use for drift correction.
+        :param position: Position in the spectrum to use for drift correction (index, string, or slice).
+        :param drift_type: Type of drift correction ('individual' or 'linear').
+        :returns: The current ImportTimeResolvedFTIR object with drift-corrected data.
         """
 
         if not isinstance(t_region, list):
@@ -183,10 +257,13 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
         return self
 
     def atmospheric_correction(self, atm_data: Spectrum, x_range=None):
-        self._orient_all_data('t')
+        """
+       Applies atmospheric correction to the data using the provided atmospheric spectrum.
 
-        atm_data_short = atm_data.truncate_to(x_range=x_range, inplace=False)
-
+       :param atm_data: The atmospheric spectrum to use for correction.
+       :param x_range: The x-range to use for correction (optional).
+       :returns: The current ImportTimeResolvedFTIR object with atmospheric correction applied.
+       """
         for data in self._data_list:
             data.atmospheric_correction(atm_data, x_range, self._pre_scans_amount)
 
@@ -194,6 +271,13 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
 
     @staticmethod
     def read_data(file, sep='\t'):
+        """
+        Reads data from a file and returns the x and y values.
+
+        :param file: The file path to read the data from.
+        :param sep: The delimiter used in the file (default is tab).
+        :returns: A tuple containing the x and y data arrays.
+        """
         x = []
         y = []
 
@@ -206,6 +290,13 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
 
     @staticmethod
     def _dod(data, pre_scans_amount):
+        """
+        Computes the delta optical density (delta-OD) from the intensity data.
+
+        :param data: The TransientSpectrum data object containing intensity data.
+        :param pre_scans_amount: The number of pre-scans to use for reference.
+        :returns: The calculated delta-OD values.
+        """
         return - np.log10(np.divide(data.y.array, data.spectrum[:pre_scans_amount].y.array))
 
     @classmethod
@@ -216,6 +307,17 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
                           x_unit: str = 'wn',
                           t_unit: str = 's',
                           sep: str = '\t'):
+        """
+        Reads a single file and returns a TransientSpectrum object.
+
+        :param file: The file to read.
+        :param pre_scans_amount: Number of pre-scans.
+        :param pre_scans_offset: Offset for the pre-scans.
+        :param x_unit: The unit for the x-axis (default is 'wn').
+        :param t_unit: The unit for the time axis (default is 's').
+        :param sep: The delimiter used in the file (default is tab).
+        :returns: A TransientSpectrum object created from the file.
+        """
         x, y = cls.read_data(file, sep)
         t = TimeAxis.from_parameters(np.shape(y)[1], 1, pre_scans_amount + pre_scans_offset, t_unit)
         return TransientSpectrum(x, t, y, x_unit=x_unit, t_unit=t_unit, data_unit='dod')
@@ -228,6 +330,17 @@ class ImportTimeResolvedFTIR(ImportTimeResolvedBase):
                    x_unit: str = 'wn',
                    t_unit: str = 's',
                    sep: str = '\t'):
+        """
+        Reads multiple files from a path and returns an ImportTimeResolvedFTIR object.
+
+        :param path: Path to the files to be read.
+        :param pre_scans_amount: Number of pre-scans.
+        :param pre_scans_offset: Offset for the pre-scans.
+        :param x_unit: The unit for the x-axis (default is 'wn').
+        :param t_unit: The unit for the time axis (default is 's').
+        :param sep: The delimiter used in the files (default is tab).
+        :returns: An ImportTimeResolvedFTIR object containing the processed data.
+        """
         from ..SpecCore.coreParser import Parser
 
         files = Parser.parse_path(path)
