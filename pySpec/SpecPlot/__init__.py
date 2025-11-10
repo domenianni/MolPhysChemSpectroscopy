acf5dfd3bd6bda17e02dc7da3620ec5eb06619c5
Markus Bauer <98280589+pentaphonic@users.noreply.github.com>
2025-11-10 15:24:14 +0100

    Remove reference to MinorSymLogLocator

--- pySpec/SpecPlot/__init__.py
+++ pySpec/SpecPlot/__init__.py
@@ -1,5 +1,4 @@
 from .plotStaticMethods import create_colormap, lighten_color, exponential_string, factor_string
-from .plotMinorSymLogLocator import MinorSymLogLocator
 from .plotFluorescence import plotFluorescence
 from .plotTransSpec import plotTransSpec, plotProdSpec, plotGlobalFit
 from .plotUVVis import plotUVVis
